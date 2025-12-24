756882340df9e648a35972c85d5c2a3e1c2e06f9
ferehan <ahmedferehan7@gmail.com>
2025-12-24 11:23:49 +0300

    to main

---
+++ ch-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Banner from './Components/Banner/Banner'
+import Catagory from './Components/Catagory/Catagory'
+import Footer from './Components/Footer/Footer'
+import Row from './Components/Row/Row'
+function App() {
+  return (
+    <div>
+
+      <Banner/>
+      <Catagory/> 
+      <Row/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ ch-app/src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Row from '../Row/Row'
+import classes from './footer.module.css'
+function Footer () {
+
+    return (
+      <div className={classes.footer}>
+<Row/>
+      </div>
+    );
+  
+}
+
+export default Footer;
