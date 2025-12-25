dab6fc0a9325ca3116a57b2159cf54a1b137b7fb
ashenafi sileshi <ashenafisileshi7@gmail.com>
2025-12-25 13:59:57 +0300

    fina-updated

--- ch-app/src/App.jsx
+++ ch-app/src/App.jsx
@@ -1,33 +1,22 @@
-<<<<<<< HEAD
-import React from 'react'
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/auth/authfront/login.jsx";
 import Banner from './Components/Banner/Banner'
 import Catagory from './Components/Catagory/Catagory'
 import Footer from './Components/Footer/Footer'
 import Row from './Components/Row/Row'
-function App() {
-  return (
-    <div>
-
-      <Banner/>
-      <Catagory/> 
-      <Row/>
-      <Footer/>
-    </div>
-  )
-}
-
-export default App
-=======
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Login from "./Components/auth/authfront/login.jsx";
-
+import Signup from "./Components/loginPage/Signup.jsx"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
+      <Banner />
+      <Catagory />
+      <Row />
+      <Footer />
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/signup" element={<Login />}/>
     </Routes>
   );
 }
 export default App;
->>>>>>> 90890e7a3327a480e8299b274f0ef185c702b944
