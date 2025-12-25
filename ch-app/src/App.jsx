e238155498959c89527ec419cb8d6e5172d5010a
ashenafi sileshi <ashenafisileshi7@gmail.com>
2025-12-25 14:06:39 +0300

    update

--- ch-app/src/App.jsx
+++ ch-app/src/App.jsx
@@ -1,11 +1,21 @@
+
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./Components/auth/authfront/login.jsx";
-
+import Banner from './Components/Banner/Banner'
+import Catagory from './Components/Catagory/Catagory'
+import Footer from './Components/Footer/Footer'
+import Row from './Components/Row/Row'
+import Signup from "./Components/loginPage/Signup.jsx"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />}/>
+      <Banner />
+      <Catagory />
+      <Row />
+      <Footer />
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/signup" element={<Login />}/>
     </Routes>
   );
 }
